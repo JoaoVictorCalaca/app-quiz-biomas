f9a56733da7bfd8a4599f0b36d9161c2f4a75187
JoaoVictorCalaca <joaovictormcalaca@gmail.com>
2024-09-12 16:42:41 -0300

    primeiro commit

---
+++ src/app/index.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, Touchable, View, TouchableOpacity, StatusBar, ScrollView } from "react-native";
+import { appColors } from "../util/colors";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <StatusBar/>
+
+      <Text style={styles.h1}>Quiz de biomas brasileiros</Text>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Jogar!</Text>
+      </TouchableOpacity>
+
+      <View>
+        <Text style={styles.h2}>🏆 ranking de jogadores</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'space-around',
+    backgroundColor: appColors.defaultBlue,
+    padding: 25,
+  },
+
+  h1: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: appColors.defaultWhite,
+    textAlign: 'center',
+    width: '90%'
+  },
+
+  h2: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: appColors.defaultWhite,
+    alignSelf: 'flex-start'
+  },
+
+  btn: {
+    backgroundColor: appColors.defaultGreen,
+    height: 70,
+    width: '100%',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  btnText: {
+    color: appColors.defaultWhite,
+    fontWeight: 'bold',
+    fontSize: 40
+  },
+})
---
+++ src/util/colors.ts
@@ -0,0 +1,10 @@
+export const appColors = {
+  defaultBlue: '#0F172A',
+  secondaryBlue: '#1D4ED8',
+  tertiaryBlue: '#020617',
+  defaultBlack: '#000103',
+  defaultWhite: '#fff',
+  secondaryWhite: '#E2E8F0',
+  defaultGreen: '#84CC16',
+  defaultRed: '#CC1616'
+}
